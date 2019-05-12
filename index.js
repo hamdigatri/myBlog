71c360e6690e55f149363253d1c9109d9ccc3c7d
hamdi <hamdigatri92@gmail.com>
2019-05-12 18:53:52 +0100

    import modules

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
+
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const { ExpressOIDC } = require("@okta/oidc-middleware");
+
 const app = new express();
 const port = 3000;
 
